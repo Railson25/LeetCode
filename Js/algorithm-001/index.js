3471a2f17e82e34f00d757b0aa7b9fad08b40cad
Railson25 <railsonsantiago123@gmail.com>
2023-10-10 14:30:19 -0300

    <feat>: Implementation 3

--- Js/algorithm-001/index.js
+++ Js/algorithm-001/index.js
@@ -18,8 +18,19 @@ console.log(resultado)
 // Formas de reduzir o codigo acima utilizando operador ternario
 
 Array.prototype.last = function() {
-    return this.length ? this[this.length - 1] : 1
+    return this.length ? this[this.length - 1] : -1
 }
 const resultado2 = minhaMatriz.last();
 
 console.log(resultado2)
+
+// Na terceira implementacao vamos gerar um novo array que contem apenas a ultima posicao do array anterior
+
+Array.prototype.last = function() {
+    // na funcao this.slice() criamos o novo array e pegamos a posicao inicial e unica [0] deste array 
+    return this.length ? this.slice(-1) [0] : -1
+}
+
+const resultado3 = minhaMatriz.last();
+
+console.log(resultado3)
